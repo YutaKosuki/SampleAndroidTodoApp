4f2dfd2cde1bc8a52db80603c936edfe50c4a040
YutaKosuki <bambiyuta@gmail.com>
2015-12-20 16:40:59 +0900

    ボタンを押したときの処理を登録

--- app/src/main/java/com/example/kosuk_000/sampleandroidtodoapp/MainActivity.java
+++ app/src/main/java/com/example/kosuk_000/sampleandroidtodoapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.kosuk_000.sampleandroidtodoapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyOpenHelper helper = new MyOpenHelper(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+
+        final EditText todoText = (EditText) findViewById(R.id.entry_todo);
+
+        Button entryButton = (Button) findViewById(R.id.btn_entry);
+        entryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todo = todoText.getText().toString();
+
+                ContentValues insertValues = new ContentValues();
+                insertValues.put("todo", todo);
+                long id = db.insert("todo", todo, insertValues);
+            }
+        });
     }
 }
